8ac64d9fc81e3102ef1e1c72dd600f192404f029
Sucheth Medicherla <mvmsucheth@gmail.com>
2024-06-30 09:17:12 +0530

    Contact us change

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -56,7 +56,17 @@ export default function CountactUs() {
         </div>
       </section>
       <section className="mb-7 ml-16">
-        <Mapbox />
+        <div>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30443.068329097256!2d78.38414625233057!3d17.489195388562898!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb91ec0162f43d%3A0x3288c6fe4358cf48!2sKukatpally%2C%20Hyderabad%2C%20Telangana!5e0!3m2!1sen!2sin!4v1719718781607!5m2!1sen!2sin"
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </section>
     </div>
   );
